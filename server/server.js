b7b18f5bb9e568e8403fd428dead63037e7e52a3
RiFlYou <scm8716@naver.com>
2025-06-29 22:40:36 +0900

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const cookieParser = require('cookie-parser'); // ✅ 추가
 require('dotenv').config();
+
 const authRoutes = require('./routes/auth');
-const app = express();
-const PORT = process.env.PORT || 5000;
 const postRoutes = require('./routes/post');
 
-app.use(cors());
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// 쿠키 파서 설정
+app.use(cookieParser());
+
+// CORS 설정 (이건 왜 하는 건지는 모르겠지만..)
+app.use(cors({
+  origin: 'http://localhost:3000', 
+  credentials: true
+}));
+
 app.use(express.json());
+
 app.use('/api/auth', authRoutes);
 app.use('/api/posts', postRoutes);
 
@@ -16,7 +28,6 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
-
 .then(() => {
   console.log('MongoDB connected');
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
